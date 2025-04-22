44ce4c482c86b474d26df0a2e3df4f8fe96a6066
abdimalik <abdimalik@gmail.com>
2025-04-22 15:12:11 +0300

    I updated entire project to React

---
+++ src/data/user_info.js
@@ -0,0 +1,146 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Abdimalik Said Elmi",
+        description: "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Full-Stack Developer",
+        photo: "../Abdimalik.jpg",
+        email: "abdimaliksaedelmi@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        twitter: "https://x.com/cabdimaaliksm",
+		github: "https://github.com/Abdimalik-Saed-Elmi?tab=repositories",
+		linkedin: "https://www.linkedin.com/in/abdimalik-said-elmi-a6424534b/",
+		resume: "https://flowcv.com/resume/v0jgigbkv4lw",
+		instagram: "https://www.instagram.com/abdimalik.saed.elmi/",
+		facebook: "https://www.facebook.com/abdimalik.saed.elmi/",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "Darulkarim School Website",
+			description: "A comprehensive website developed by me for the Darulkarim School Website, built using the MERN stack to manage student records, course information, school events.",
+			technologies: 'MongoDB, Express.js, React, Node.js, Tailwind',
+			github: "https://github.com/dsbalico/YouShare",
+            link: "https://github.com/dsbalico/YouShare"
+		},
+
+        {
+			title: "WhatsApp Link Generator",
+			description: "Lets you generate WhatsApp links for free with name and QR code in less than 1 minute, no registration required.",
+            technologies: "React, Tailwind, JavaScript",
+			github: "https://github.com/Abdimalik-Saed-Elmi/WhatsApp-Link-Generator-With-React",
+            link: "https://whats-app-link-generator.vercel.app/"
+		},
+
+
+        {
+			title: "Built Twenty Five Plus Five Clock",
+			description: "I developed a Twenty Five Plus Five Clock, a digital timer implementing the pomodoro technique. Users can set their desired work session length (initially 25 minutes) and break session length (initially 5 minutes).",
+			technologies: "React, Tailwind, TypeScript",
+			github: "https://github.com/Abdimalik-Saed-Elmi/Build-Twenty-Five-Plus-Five-Clock-FCC",
+            link: "https://build-twenty-five-plus-five-clock-fcc.vercel.app/"
+		},
+
+		{
+			title: "Built Cash Register",
+			description: "This project is a functional web-based cash register. It allows users to input a purchase price and the amount of cash given, then calculates and displays the correct change to be returned, broken down into specific denominations.",
+			technologies: "React, Tailwind, JavaScript",
+			github: "https://github.com/Abdimalik-Saed-Elmi/Build-Cash-Register-FreeCodeCamp-Certificate",
+            link: "https://build-cash-register-freecodecamp.vercel.app/"
+		},
+
+		{
+			title: "Full Authentication System",
+			description: "This project is a comprehensive full authentication system, providing secure user registration, login, and session management.",
+			technologies: 'MongoDB, Express.js, React, Node.js, Tailwind',
+			github: "https://github.com/Abdimalik-Saed-Elmi/Mern-Stack-Full-Authentication-System",
+            link: "https://full-authentication-q8ul.onrender.com/"
+		},
+
+		{
+			title: "Markdown Previewer Project",
+			description: "The Markdown Previewer is a dynamic web application meticulously crafted using React for the frontend, core TypeScript for logic, leveraging HTML for structure, and styled with Tailwind.",
+			technologies: "React, Tailwind, TypeScript",
+			github: "https://github.com/Abdimalik-Saed-Elmi/Markdown-Previewer-Freecodecamp-Certificate-Project",
+            link: "https://markdown-previewer-freecodecamp-one.vercel.app/"
+		},
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Salaam University",
+            degree: "Bachelor of Science in Computer Science",
+            duration: "2020 - 2024",
+            image: "Salaam logo.jpg"
+        },
+        {
+            school: "Darulkarim School",
+            degree: "Diploma in High School/ Secondary School",
+            duration: "2014 - 2020",
+            image: "Darulkarim.PNG"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Teacher of ICT and Mathematics",
+            company: "Darulkarim Primary & Secondary Shcool",
+            duration: "august 2019 - Present (above 5years)",
+            image: "Darulkarim.PNG",
+            descriptions: [
+                "Maintain a positive and supportive learning environment",
+                "That encourages student curiosity, exploration, and a love of learning in Mathematics and ICT.",
+                "Promotes active exploration of mathematical concepts and ICT tools",
+            ]
+        }
+        // {
+        //     position: "Work Immersion - Web Developer",
+        //     company: "St. Peter Velle Technical Training Center",
+        //     duration: "Nov 2019 - Dec 2019 (1 Month)",
+        //     image: "spvttc.webp",
+        //     descriptions: [
+        //         "Developed Python scripts to automate document score calculations.",
+        //         "Developed a company website as part of an ICT-related work immersion program."
+        //     ]
+        // },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Responsive Web Design Certificate",
+            description: "Provided by FreeCodeCamp and Authorized by freeCodeCamp.org.",
+            icon: "freecodecamp",
+            link: "https://www.freecodecamp.org/certification/Abdimalik-Said-Elmi/responsive-web-design",
+        },
+        {
+            title: "JavaScript Algorithms and Data Structures Certificate",
+            description: "Provided by FreeCodeCamp and Authorized by freeCodeCamp.org.",
+            icon: "freecodecamp",
+            link: "https://www.freecodecamp.org/certification/Abdimalik-Said-Elmi/javascript-algorithms-and-data-structures-v8",
+        },
+        {
+            title: "Front End Development Libraries Certificate",
+            description: "Provided by FreeCodeCamp and Authorized by freeCodeCamp.org.",
+            icon: "freecodecamp",
+            link: "https://www.freecodecamp.org/certification/Abdimalik-Said-Elmi/front-end-development-libraries"
+        },
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at abdimaliksaedelmi@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2025 Abdimalik Said ELmi. All Rights Reserved"
+}
+
+export default info
